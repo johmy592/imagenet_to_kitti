6c2183a2a456369459f6cc51bb0e6b5053b0c3d0
johmy592 <johmy592@student.liu.se>
2017-08-01 11:38:16 +0200

    Now creates correct folder structure and copies images aswell as creating label files

--- converter.py
+++ converter.py
@@ -1,3 +1,12 @@
+# Creates a dataset in KITTI format from Imagenet data with bounding boxes
+# in the PASCALVOC format. 
+#
+#  NOTE: Only training data is created. If validation data is desired
+#        one can for example manually move some label files and 
+#        corresponding images to a validation folder. 
+
+
+
 import argparse
 import sys
 import getopt
@@ -8,7 +17,12 @@ import xml.etree.ElementTree as ET
 
     
 
+""" Parses the XML-files from the PASCALVOC data and writes .txt label
+files in the KITTI format.
 
+NOTE: Only fields used by DIGITS to train a detectnet model are written.
+      Other fields are set to 0  
+"""
 def _parse_xml(root, to_path):
     annotation_path = "%s/Annotations" % (root)
     xml_files = [f for f in os.listdir(annotation_path)]
@@ -30,10 +44,28 @@ def _parse_xml(root, to_path):
             text_file.close
         
 
+def _copy_images(root, images_dir):
+    img_src = '%s/JPEGImages' % (root)
+    image_files = [f for f in os.listdir(img_src)]
+    for img in image_files:
+        shutil.copy2('%s/%s' % (img_src,img), images_dir)    
+
+def _create_dirs(to_path):
+    train_dir = '%s/%s' % (to_path,'train')
+    images_dir = '%s/%s' % (train_dir,'images')
+    labels_dir = '%s/%s' % (train_dir,'labels')
+    if not os.path.exists(train_dir):
+        os.makedirs(train_dir)
+        os.makedirs(images_dir)
+        os.makedirs(labels_dir)
+    elif not os.path.exists(images_dir) and not os.path.exists(labels_dir):
+        os.makedirs(images_dir)
+        os.makedirs(labels_dir)
+
+    return labels_dir,images_dir
+
 if __name__ == '__main__':
-    #from_path = sys.argv[1]
-    #to_path = sys.argv[2]
-    
+        
     try:
         opts, args = getopt.getopt(sys.argv[1:],"hf:t:",["from=","to="])
     except getopt.GetoptError:
@@ -42,11 +74,12 @@ if __name__ == '__main__':
     for opt, arg in opts:
         if opt == '-h':
             print 'Converter.py -f <from-folder> -t <to-folder> '
-            print 'from-folder: root of PASCALVOC data, to-folder: where kitti label files should be written'
+            print 'from-folder: root of PASCALVOC data, to-folder: folder where kitti data will be created'
             sys.exit()
         elif opt in('-f','--from'):
             from_path = arg
         elif opt in ('-t', '--to'):
             to_path= arg
-     
-    _parse_xml(from_path, to_path)    
+    labels_dir, images_dir = _create_dirs(to_path)     
+    _parse_xml(from_path, labels_dir)
+    _copy_images(from_path, images_dir)    
